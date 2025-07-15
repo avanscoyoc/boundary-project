6431e6150da06bb72ed7fbc4dcf82016577198bb
avanscoyoc <amy.e.van.scoyoc@gmail.com>
2025-07-15 20:08:12 +0000

    added example tif files, updates to manuscript.md text to reflect new methods, added example2 for rolling mean/breakpoint analysis (incomplete)

--- src/config.py
+++ src/config.py
@@ -4,22 +4,11 @@ import pandas as pd
 import geopandas as gpd
 import json
         
-def remove_high_perimeter_area_ratio(features):
-    """Calculate perimeter to area ratio and filter out narrow features in top 25%"""
-    features_with_ratio = features.map(lambda f: f.set({
-        'perimeter_area_ratio': f.geometry().perimeter().divide(f.get('GIS_AREA'))
-    }))
-    ratio_stats = features_with_ratio.aggregate_stats('perimeter_area_ratio')
-    ratio_threshold = ratio_stats.get('p75')
-    narrow_filter = ee.Filter.lt('perimeter_area_ratio', ratio_threshold)
-    
-    return features_with_ratio, narrow_filter
 
 def load_protected_area(name):
     """Load a protected area by name from the WDPA dataset"""
     protected_areas = ee.FeatureCollection("WCMC/WDPA/202106/polygons")
     
-    # Define basic filters
     marine_filter = ee.Filter.eq("MARINE", "0")
     not_mpa_filter = ee.Filter.neq("DESIG_ENG", "Marine Protected Area")
     status_filter = ee.Filter.inList("STATUS", ["Designated", "Established", "Inscribed"])
@@ -29,8 +18,6 @@ def load_protected_area(name):
                     "555665485", "555556142", "187", "555703455", "555563456", "15894"]
     pids_filter = ee.Filter.inList("WDPA_PID", excluded_pids).Not()
    
-    
-    # Combine all filters
     combined_filter = ee.Filter.And(
         marine_filter,
         not_mpa_filter,
@@ -41,7 +28,6 @@ def load_protected_area(name):
        
     )
 
-    # Filter and get specific protected area
     data = protected_areas.filter(combined_filter)
     pa = data.filter(ee.Filter.eq('WDPA_PID', name)).first()
     
