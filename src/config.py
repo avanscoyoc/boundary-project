0197dd59e36244ecc115fd669e5bcc43aea154cc
avanscoyoc <amy.e.van.scoyoc@gmail.com>
2025-07-30 17:20:19 +0000

    added movement notebook

--- src/config.py
+++ src/config.py
@@ -5,7 +5,7 @@ import geopandas as gpd
 import json
         
 
-def load_protected_area(name):
+def load_protected_area():
     """Load a protected area by name from the WDPA dataset"""
     protected_areas = ee.FeatureCollection("WCMC/WDPA/202106/polygons")
     
@@ -13,25 +13,25 @@ def load_protected_area(name):
     not_mpa_filter = ee.Filter.neq("DESIG_ENG", "Marine Protected Area")
     status_filter = ee.Filter.inList("STATUS", ["Designated", "Established", "Inscribed"])
     designation_filter = ee.Filter.neq("DESIG_ENG", "UNESCO-MAB Biosphere Reserve")
-    area_filter = ee.Filter.gte("GIS_AREA", 200)
-    excluded_pids = ["555655917", "555656005", "555656013", "555665477", "555656021",
-                    "555665485", "555556142", "187", "555703455", "555563456", "15894"]
-    pids_filter = ee.Filter.inList("WDPA_PID", excluded_pids).Not()
+    area_filter = ee.Filter.gte("GIS_AREA", 100)
+    #excluded_pids = ["555655917", "555656005", "555656013", "555665477", "555656021",
+    #                "555665485", "555556142", "187", "555703455", "555563456", "15894"]
+    #pids_filter = ee.Filter.inList("WDPA_PID", excluded_pids).Not()
    
     combined_filter = ee.Filter.And(
         marine_filter,
         not_mpa_filter,
         status_filter,
         designation_filter,
-        pids_filter,
+    #    pids_filter,
         area_filter
        
     )
 
     data = protected_areas.filter(combined_filter)
-    pa = data.filter(ee.Filter.eq('WDPA_PID', name)).first()
+    #pa = data.filter(ee.Filter.eq('WDPA_PID', name)).first()
     
-    return pa
+    return data
 
 def load_local_data(wdpa_id):
     """Load protected area from local shapefile and convert to EE Feature"""
