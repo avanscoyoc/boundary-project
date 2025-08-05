6019498767141dd15f123cc0b60f3ca2f9e22767
avanscoyoc <amy.e.van.scoyoc@gmail.com>
2025-08-05 20:54:22 +0000

    ready for 851 movement sites

--- src/main.py
+++ src/main.py
@@ -10,9 +10,9 @@ ee.Initialize(project='dse-staff')
 
 def main():
     start = time.time()
-    
+
     sites = ee.FeatureCollection("projects/dse-staff/assets/movement_metadata").geometry().buffer(50000) #50km
-    wdpaids = pas_at_movement_sites(sites)[:5]
+    wdpaids = pas_at_movement_sites(sites)
 
     run_all(wdpaids, start_year=2016, n_years=1, max_concurrent=15)
 
